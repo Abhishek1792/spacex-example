7d163d7f3095d74c4477b926c417ec5393cdf308
abhishek <abhishek.kalia@indianic.com>
2023-06-26 15:05:10 +0530

    more marker added

--- src/screens/main/LaunchMap/index.tsx
+++ src/screens/main/LaunchMap/index.tsx
@@ -50,8 +50,35 @@ const LaunchMap = () => {
         latitude: 37.75,
         longitude: -122.45
       },
-      title: "",
-      description: ""
+      title: "LZ-1",
+      description: "SpaceX's first east coast landing pad is Landing Zone 1, where the historic first Falcon 9 landing occurred in December 2015. LC-13 was originally used as a launch pad for early Atlas missiles and rockets from Lockheed Martin. LC-1 was later expanded to include Landing Zone 2 for side booster RTLS Falcon Heavy missions, and it was first used in February 2018 for that purpose."
+    },
+    {
+      latlng: {
+        latitude: 37.78,
+        longitude: -122.41
+      },
+      title: "LZ-2",
+      description: "SpaceX's first east coast landing pad is Landing Zone 1, where the historic first Falcon 9 landing occurred in December 2015. LC-13 was originally used as a launch pad for early Atlas missiles and rockets from Lockheed Martin. LC-1 was later expanded to include Landing Zone 2 for side booster RTLS Falcon Heavy missions, and it was first used in February 2018 for that purpose."
+
+    },
+    {
+      latlng: {
+        latitude: 37.76,
+        longitude: -122.43
+      },
+      title: "LZ-3",
+      description: "SpaceX's west coast landing pad. The pad is adjacent to SLC-4E, SpaceX's west coast launch site. The pad was under construction for about a year starting in 2016. After concerns with seal mating season, this pad was first used for the SAOCOM 1A mission in October 2018. Officially referred to as LZ-4 in FCC filings."
+
+    },
+    {
+      latlng: {
+        latitude: 37.75,
+        longitude: -122.45
+      },
+      title: "LZ-4",
+      description: "The second ASDS barge, Of Course I Still Love You (OCISLY), had been under construction in a Louisiana shipyard since early 2015 using a different hull—Marmac 304—in order to service launches on the east coast. It was built as a replacement for the first Just Read the Instructions and entered operational service for Falcon 9 Flight 19 in late June 2015. As of June 2015, its home port was Jacksonville, Florida, but after December 2015, it was transferred 160 miles (260 km) further south, at Port Canaveral. While the dimensions of the ship are nearly identical to the first ASDS, several enhancements were made including a steel blast wall erected between the aft containers and the landing deck. The ship was in place for a first-stage landing test on the CRS-7 mission, which failed on launch on 28 June 2015. On 8 April 2016 the first stage, which launched the Dragon CRS-8 spacecraft, successfully landed for the first time ever on OCISLY, which is also the first ever drone ship landing. In February 2018, the Falcon Heavy Test Flight's central core exploded upon impact next to OCISLY that damaged two of the four thrusters on the drone ship. Two thrusters were removed from the Marmac 303 barge in order to repair OCISLY."
+
     }
   ]
 
